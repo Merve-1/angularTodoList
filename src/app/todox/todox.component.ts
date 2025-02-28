23aa82985e0c627a12c7855f50cc7337b26ccabc
Marwa Alazab <104984704+Merve-1@users.noreply.github.com>
2025-03-01 00:28:53 +0200

    updates

--- src/app/todox/todox.component.ts
+++ src/app/todox/todox.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodoxService } from '../services/todox.service';
 import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-todox',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './todox.component.html',
   styleUrl: './todox.component.scss'
 })
@@ -13,7 +15,15 @@ export class TodoxComponent implements OnInit {
   todoItems = signal<Array<Todo>>([]);
 
   ngOnInit(): void {
-    console.log(this.todoService.todoItems);
-    this.todoItems.set(this.todoService.todoItems);
+
+    this.todoService.getTodoxFromApi().pipe(
+      catchError((err) =>
+      {
+          console.log(err);
+          throw err;
+      })
+    ).subscribe((todox) =>{
+      this.todoItems.set(todox);
+    })
   }
 }
